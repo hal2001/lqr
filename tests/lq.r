e9d9172f9f8b62278da88f8a6c5937f5481069cc
wrathematics <wrathematics@gmail.com>
2017-03-02 10:11:34 -0500

    oopsie

--- tests/lq.r
+++ tests/lq.r
@@ -1,45 +1,34 @@
 library(lqr)
 set.seed(1234)
 
-m = 2
+test = function(x)
+{
+  L = LQ(x, retq=FALSE)
+  Q = LQ(x, retl=FALSE)
+  stopifnot(all.equal(L%*%Q, x))
+
+  Qt = t(Q)
+  R = t(L)
+
+  QR = qr(t(x))
+  Q_R = qr.Q(QR)
+  R_R = qr.R(QR)
+  stopifnot(all.equal(Qt, Q_R))
+  stopifnot(all.equal(R, R_R))
+}
+
+m = 5
 n = 5
 x = matrix(rnorm(m*n), m, n)
-
-L = LQ(x, retq=FALSE)
-Q = LQ(x, retl=FALSE)
-stopifnot(all.equal(L%*%Q, x))
-
-Qt = t(Q)
-R = t(L)
-
-QR = qr(t(x))
-Q_R = qr.Q(QR)
-R_R = qr.R(QR)
-stopifnot(all.equal(Qt, Q_R))
-stopifnot(all.equal(R, R_R))
+test(x)
 
 
 
 m = 10
 n = 3
 x = matrix(rnorm(m*n), m, n)
-
-Q1 = QR(x, retr=FALSE)
-Q2 = qr.Q(qr(x))
-stopifnot(all.equal(Q1, Q2))
-
-R1 = QR(x, retq=FALSE)
-R2 = qr.R(qr(x))
-stopifnot(all.equal(R1, R2))
-
+test(x)
 
 
 x = t(x)
-
-Q1 = QR(x, retr=FALSE)
-Q2 = qr.Q(qr(x))
-stopifnot(all.equal(Q1, Q2))
-
-R1 = QR(x, retq=FALSE)
-R2 = qr.R(qr(x))
-stopifnot(all.equal(R1, R2))
+test(x)
