dc017985068ccb248e0dafde83d74a5e7e184a30
wrathematics <wrathematics@gmail.com>
2017-03-02 10:29:54 -0500

    add some benchmarks

---
+++ inst/benchmarks/qr.r
@@ -0,0 +1,17 @@
+library(lqr)
+library(rbenchmark)
+
+rqr <- function(x)
+{
+  tmp <- qr(x, LAPACK=TRUE)
+  list(Q=qr.Q(tmp), R=qr.R(tmp))
+}
+
+cols <- cols <- c("test", "replications", "elapsed", "relative")
+reps <- 25
+
+m = 2000
+n = 500
+x = matrix(rnorm(m*n), m, n)
+
+benchmark(QR(x), rqr(x), replications=reps, columns=cols)
---
+++ inst/benchmarks/r.r
@@ -0,0 +1,13 @@
+library(lqr)
+library(rbenchmark)
+
+rqr <- function(x) qr.R(qr(x, LAPACK=TRUE))
+
+cols <- cols <- c("test", "replications", "elapsed", "relative")
+reps <- 25
+
+m = 2000
+n = 500
+x = matrix(rnorm(m*n), m, n)
+
+benchmark(QR(x, retq=FALSE), rqr(x), replications=reps, columns=cols)
